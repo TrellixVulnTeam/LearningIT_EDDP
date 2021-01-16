85ee54c2e86279e194e700d3d00d518faea026f4
COSTIN ALEXANDRU FRATILA <costin.fratila@s.unibuc.ro>
2021-01-16 13:58:45 +0200

    Update Badges

--- BackEnd/Learning IT/Models/Exam.cs
+++ BackEnd/Learning IT/Models/Exam.cs
@@ -19,8 +19,6 @@ namespace Learning_IT.Models
         public string Content { get; set; }
         [Column(TypeName = "decimal(9,2)")]
         public Decimal Points { get; set; }
-
-
         public int ChapterId { get; set; }
 
         public Chapter Chapter { get; set; }
